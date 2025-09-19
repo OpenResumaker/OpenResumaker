9ed6d2d41ca717db982c7178e54510c80a7085ce
MilkWind <2250957906@qq.com>
2025-09-19 19:40:52 +0800

    optimize: 优化文本

--- src/components/page-layout/ActionButtons.tsx
+++ src/components/page-layout/ActionButtons.tsx
@@ -42,7 +42,7 @@ export const ActionButtons = ({
         className="flex items-center space-x-2 text-blue-600 hover:text-blue-700 hover:bg-blue-50"
       >
         <Settings className="h-4 w-4" />
-        <span className="hidden sm:inline">管理模块</span>
+        <span className="hidden sm:inline">模块管理</span>
       </Button>
 
       <Button
